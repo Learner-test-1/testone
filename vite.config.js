30374cf5c8e63ee0fcba5b888ac4b4e539b5e15c
Learner test <learnertest581@gmail.com>
2025-12-30 23:19:10 +0530

    addeddddddddd

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const base = import.meta.env.VITE_BASE_PATH;
   return {
+    
     plugins: [react()],
     base: base || "/",
   };
